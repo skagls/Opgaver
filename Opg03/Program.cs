ba1e1b45c9b8fb3e54bbba2c29d05a58718af546
skagls <ska@gls-denmark.com>
2017-04-28 14:41:49 +0200

    .

--- Opg03/Program.cs
+++ Opg03/Program.cs
@@ -29,7 +29,8 @@ namespace Opg03
 
             Array.Sort(månedsløn);
             Console.ReadKey();
-
+           
+         
         }
     }
 }
---
+++ Opg04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fornavn = "Mikkel";
+            string efternavn = "Cronberg";
+            string samletnavn = $"{fornavn} {efternavn}";
+            Console.WriteLine(samletnavn);
+
+            string navnStort = samletnavn.ToUpper();
+            Console.WriteLine(navnStort);
+            string navnLille = samletnavn.ToLower();
+            Console.WriteLine(navnLille);
+
+            String del = samletnavn.Substring(7, 4);
+            Console.WriteLine(del);
+
+            String[] navne = samletnavn.Split(' ');
+            foreach (var item in navne)
+            Console.WriteLine(item);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
