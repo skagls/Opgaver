e1cb36f44de9963c3bf06324e5d9f0be47922bcd
skagls <ska@gls-denmark.com>
2017-05-08 15:01:50 +0200

    .

---
+++ Opg09/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terning t1;
+            t1 = new Terning();
+            Console.WriteLine(t1.Værdi);
+            Terning t2;
+            t2 = new Terning(2);
+            Console.WriteLine(t2.Værdi);
+            t2 = t1;
+            Console.WriteLine(t1.Værdi);
+            Console.WriteLine(t2.Værdi);
+            t1.Værdi = 1;
+            Console.WriteLine(t1.Værdi);
+            Console.WriteLine(t2.Værdi);
+            t2.Værdi = 6;
+            Console.WriteLine(t1.Værdi);
+            Console.WriteLine(t2.Værdi);
+            
+        }
+    }
+    class Terning
+    {
+        public int Værdi;
+        public Terning ()
+        {
+            this.Værdi = new Random().Next(1, 7);
+        }
+        public Terning (int Værdi)
+        {
+            this.Værdi = Værdi;
+        }
+        
+    }
+}
