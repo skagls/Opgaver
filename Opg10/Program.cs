4ea1c0e636a2540c7de3443000ff6551cb1b219c
skagls <ska@gls-denmark.com>
2017-05-08 15:38:24 +0200

    1

---
+++ Opg10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Terning t1 = new Terning();
+            t1.Skriv();
+            t1.Ryst();
+            t1.Skriv();
+            Terning t2 = new Terning(true);
+            t2.Skriv();
+            t2.Ryst();
+            t2.Skriv();
+
+        }
+    }
+}
---
+++ Opg10/Terning.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg10
+{
+    public class Terning
+    {
+        private static Random rnd = new Random();
+        public int værdi;
+        private bool snyd;
+
+        public Terning ()
+        {
+            this.værdi = 1;
+            this.snyd = false;
+        }
+        public Terning(bool snyd)
+        {
+            this.værdi = 1;
+            this.snyd = snyd;
+        }
+        public void Skriv()
+        {
+            Console.WriteLine("["+this.værdi+"]");
+        }
+        public void Ryst()
+        {
+            if (snyd)
+                this.værdi = 6;
+            else
+                this.værdi = rnd.Next(1, 7);
+        }
+    }
+}
