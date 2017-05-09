0d9228fed67f2474b3e79658bd3a6d7427540372
skagls <ska@gls-denmark.com>
2017-05-09 15:07:42 +0200

    .

---
+++ Opg14/LudoTerning.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg14
+{
+    public class LudoTerning:Terning
+    {
+        public bool ErGlobus()
+        {
+            return this.Værdi == 3;
+        }
+        public bool ErStjerne()
+        {
+            return this.Værdi == 5;
+        }
+        public LudoTerning() : base()
+        { }
+        public LudoTerning(int værdi):base(værdi)
+        { }
+      
+    }
+}
---
+++ Opg14/Terning.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg14
+{
+    public class Terning
+    {
+        private static System.Random rnd;
+        private int værdi;
+        public int Værdi
+        { get
+            { Console.WriteLine("Terning aflæses som" + this.værdi);
+                return this.værdi;
+            }
+            set
+            {
+                if (value < 0 || value > 6)
+                    value = 1;
+                Console.WriteLine("Terning toldeles" + value);
+                this.værdi = value;
+                
+            }
+        }
+        public void Skriv()
+        { Console.WriteLine("[" +this.Værdi+ "]");
+        }
+        public void Ryst()
+        { this.Værdi = rnd.Next(1, 7);
+        }
+        public Terning ()
+        {
+            this.Ryst();
+        }
+        public Terning (int værdi)
+        {
+            this.Værdi = værdi;
+        }
+        static Terning()
+        {
+            rnd = new Random();
+        }
+    
+    }
+}
