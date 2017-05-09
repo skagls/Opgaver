495dd88cfb60710821aa21093ac66fc1550e4941
skagls <ska@gls-denmark.com>
2017-05-09 11:20:49 +0200

    .

---
+++ Opg12/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg12
+{
+    class Person
+    {
+        public string Fornavn { get; set; }
+        private string efternavn;
+
+        public string Efternavn
+        {
+            get { return efternavn; }
+            set {
+                if (value.Length < 3)
+
+                {
+                    efternavn = " ";
+                }
+                efternavn = value;
+            }
+        }
+
+        public string FuldtNavn()
+        {
+            return Fornavn + " " + Efternavn;
+
+        }
+
+
+
+    }
+}
---
+++ Opg12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Fornavn = "Senthuran";
+            p.Efternavn = "Kanthasamy";
+            Console.WriteLine(p.FuldtNavn());
+
+
+        }
+    }
+}
