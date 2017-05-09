878cda314ed9597744793ad821b40f551d7c41c1
skagls <ska@gls-denmark.com>
2017-05-09 10:41:12 +0200

    .

---
+++ Opg11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vare v = new Vare();
+            v.Navn = "vare # 1";
+            v.Pris = 100;
+            Console.WriteLine(v.PrisMedMoms());
+            Vare v2 = new Vare("vare #2", 200);
+            Console.WriteLine(v2.PrisMedMoms());
+
+        }
+    }
+}
