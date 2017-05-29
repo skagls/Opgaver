52a89d26d5392126297cafcca406a0e8baec2e72
admin <admin@AR-113-01>
2017-05-29 11:19:41 +0200

    .

---
+++ Opg 15 fejlhåndtering/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg_15_fejlhåndtering
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try
+
+            {
+                Run();
+            }
+            catch(Exception ex)
+            { Console.WriteLine("Der er sket en fejl "+ ex.Message);
+            }
+
+
+        }
+        private static void Run()
+        {
+            Console.WriteLine("Indtast 1.tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast 2. tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            int res = tal1 + tal2;
+            Console.WriteLine("resultater er" + res);
+        
+        }
+     
+    }
+}
