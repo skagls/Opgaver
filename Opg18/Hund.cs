3e9829b26ee4fe0a32895b1f8283a1b5c1d40f07
admin <admin@AR-113-01>
2017-05-29 15:55:19 +0200

    .

---
+++ Opg18/Hund.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg18
+{
+    class Hund : IComparable
+    { 
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            //throw new NotImplementedException();
+
+            Hund h = obj as Hund;
+        if (h.Alder > this.Alder)
+                return -1;
+            if (h.Alder < this.Alder)
+                return -1;
+            return 0; 
+        }
+    
+        
+        
+    }
+}
---
+++ Opg18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg18
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Hund[] hunde = new Hund[2];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn);
+            }
+          
+        }
+    }
+}
