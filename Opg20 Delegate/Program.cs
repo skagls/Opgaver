c8ab41806901221e4ad9a1d9e760d90e5ae0806f
admin <admin@AR-113-01>
2017-05-30 13:24:11 +0200

    .

---
+++ Opg20 Delegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg20_Delegate
+{
+    class Program
+    {
+
+        public delegate int BeregnDelegate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            int res = Beregner(1, 1, Plus);
+            res = Beregner(1, 1, Minus);
+            
+        }
+
+        public static int Beregner(int a , int b, BeregnDelegate funktion)
+        {
+            return funktion(a, b);
+
+        }
+
+        public static int Plus (int a, int b)
+        {
+            return a + b;
+        }
+        public static int Minus (int a, int b)
+        {
+            return a - b;
+           
+        }
+        public static int Divider (int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Gange (int a, int b)
+        {
+            return a * b;
+
+        }
+        
+    }
+}
